5d4b34a17db2b2389ba808b821ceedfab2b7dc3b
sooshiance <s.katafract@gmail.com>
2024-07-29 09:14:30 +0330

    build: Answer the 4Sum question

---
+++ 4sum/py_main.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        n = len(nums)
+        res = []
+        
+        for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            for j in range(i+1, n-2):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                
+                left = j + 1
+                right = n - 1
+                
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    
+                    if total == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        
+                        while left < right and nums[left] == nums[left+1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1
+                        
+                        left += 1
+                        right -= 1
+                    
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+        
+        return res
+
+
+sol = Solution()
+
+example_input = [1,0,-1,0,-2,2]
+t = 0
+
+print(sol.fourSum(example_input, t))
